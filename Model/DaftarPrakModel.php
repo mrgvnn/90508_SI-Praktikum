c6eb5a626cc50ce1e9791b962a1676a1fadb4f01
Samira Rahma Aziza <mrgvnn13@gmail.com>
2021-04-19 19:25:48 +0700

    7.9 Mengubah file DaftarPrakModel.php

--- Model/DaftarPrakModel.php
+++ Model/DaftarPrakModel.php
@@ -1,5 +1,28 @@
 <?php
-class DaftarPrakModel{
-
-
+class DaftarPrakModel
+{
+    /**Function get berfungsi untuk mengambil seluruh data praktikum yang telah mendaftar praktikum */
+    public function get()
+    {
+        $sql = "SELECT daftarprak.id as idDaftar , daftarprak.praktikan_id as id_Praktikan , praktikan.nama as namaPraktikan , daftarprak.status as status, praktikum.nama as namaPraktikum FROM daftarprak
+         JOIN praktikan ON praktikan.id = daftarprak.praktikan_id
+         JOIN praktikum ON praktikum.id = daftarprak.praktikum_id
+         WHERE praktikum.status = 1";
+         $query = koneksi()->query($sql);
+         $hasil = [];
+         while($data = $query->fetch_assoc()){
+             $hasil [] = $data;
+         }
+         return $hasil;
+    }
+    /** Function index berfungsi untuk mengatur tampilan halaman awal daftar */
+    public function index(){
+        $data = $this->get();
+        extract($data);
+        require_once("Vie/daftarprak/index.php");
+    }
 }
+
+//$tes = new DaftarPrakModel();
+//var_export($tes->get());
+//die();
