4de802cdeec7644ebb16bb4134ede9e85b19ea0a
Samira Rahma Aziza <mrgvnn13@gmail.com>
2021-04-19 19:24:22 +0700

    7.7 Mengubah file PraktikumModel.php

--- Model/PraktikumModel.php
+++ Model/PraktikumModel.php
@@ -1,5 +1,26 @@
 <?php
-class PraktikumModel{
+class PraktikumModel
+{
+    /** Function berfungsi untuk mengambil seluruh data pada database */
+    public function get(){
+        $sql = "SELECT * FROM praktikum";
+        $query = koneksi()->query($sql);
+        $hasil = [];
+        while($data = $query->fetch_assoc()) {
+            $hasil[] = $data;
+        }
+        return $hasil;
+    }
 
+      /** Function index berfungsi untuk mengatur tampilan awal */
+      public function index(){
+        $data = $this->get();
+        extract($data);
+        require_once("View/praktikum/index.php");
+    }
 
 }
+
+$tes = new PraktikumModel();
+var_export($tes->get());
+die();
