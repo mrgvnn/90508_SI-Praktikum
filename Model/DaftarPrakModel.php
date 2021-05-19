764938357a4eca34332fe286ad4181f111d50cac
Samira Rahma Aziza <mrgvnn13@gmail.com>
2021-05-19 20:29:20 +0700

    8.1.5. Ubah file DaftarprakModel.php

--- Model/DaftarPrakModel.php
+++ Model/DaftarPrakModel.php
@@ -21,8 +21,69 @@ class DaftarPrakModel
         extract($data);
         require_once("View/daftarprak/index.php");
     }
+
+
+/**
+ * @param integer $id berisi id
+ * @param integer idAslab berisi id aslab
+ * function prosesVerif berfungsi untuk mengupdate status pada database yang telah diverifikasi
+ */
+
+public function prosesVerif($id, $idAslab)
+{
+    $sql = "UPDATE daftarprak SET status=1, aslab_id = $idAslab where id = $id";
+    $query = koneksi()->query($sql);
+    return $query;
 }
 
+
+/**
+ * @param integer id berisi id
+ * @param integer idPraktikan berisi id praktikan
+ * function unverif untuk membatalkan status verifikasi
+ * 
+ */
+
+public function prosesUnVerif($id, $idPraktikan)
+{
+    $sqlDelete = "DELETE FROM nilai where praktikan_id = $idPraktikan";
+    $query = koneksi()->query($sqlDelete);
+    $sqlUpdate = "UPDATE daftarprak SET status=0, aslab_id = NULL where id=$id";
+    $query = koneksi()->query($sqlUpdate);
+    return $query;
+}
+
+/**
+ * function verif berfungsi untuk memverifikasi praktikan yang sudah mendaftar praktikum
+ */
+
+public function verif()
+{
+    $id = $_GET['id'];
+    $idAslab = $_GET['aslab']['id'];
+    if($this->prosesVerif($id, $idAslab)){
+        header("location: index.php?page=daftarprak&aksi=view&pesan=Berhasil Verif Praktikan");
+    }else{
+        header("location: index.php?page=daftarprak&aksi=view&pesan=Gagal Verif Praktikan");
+    }
+}
+
+/**
+ * function Unverif digunakan untuk membatalkan verifikasi
+ */
+
+public function unVerif()
+{
+    $id = $_GET['id'];
+    $idAslab = $_GET['idPraktikan'];
+    if($this->prosesUnVerif($id, $idPraktikan)){
+        header("location: index.php?page=daftarprak&aksi=view&pesan=Berhasil Verif Praktikan");
+    }else{
+        header("location: index.php?page=daftarprak&aksi=view&pesan=Gagal Verif Praktikan");
+    }
+}
+
+}
 //$tes = new DaftarPrakModel();
-//var_export($tes->get());
+//var_export($tes->prosesUnVerif(2,2));
 //die();
