2d70865e6b006676c25eacdfcd39d6f60fd3d5bf
Samira Rahma Aziza <mrgvnn13@gmail.com>
2021-05-19 20:30:50 +0700

    8.1.7. Ubah file AuthModel.php

--- Model/AuthModel.php
+++ Model/AuthModel.php
@@ -1,82 +1,120 @@
 <?php
 class AuthModel
 {
-    /** Function index berfungsi untuk mengatur tampilan awal */
-    public function index(){
+    /**
+     * function index fungsinya untuk mengatur tampilan awal
+     **/
+public function index()
+    {
         require_once("View/auth/index.php");
-    }
-
-    /** Function login_aslab berfungsi untuk mengatur halaman login aslab */
-    public function login_aslab(){
+    }   
+    /**
+     * function login_aslab fungsinya untuk mengatur ke halaman login untuk aslab
+     **/
+    public function login_aslab()
+    {
         require_once("View/auth/login_aslab.php");
-    }
 
-    /** Function login_aslab berfungsi untuk mengatur halaman login praktikan */
-    public function login_praktikan(){
+    }
+    /**
+     * function login_praktikan fungsinya untuk mengatur ke halaman login untuk praktikan
+     **/
+    public function login_praktikan()
+    {
         require_once("View/auth/login_praktikan.php");
     }
-
-    /** Function login_aslab berfungsi untuk mengatur halaman tampilan daftar */
-    public function daftarPraktikan(){
+    /**
+     * function daftar_praktikan fungsinya untuk mengatur tampilan daftar
+     **/
+    public function daftar_praktikan()
+    {
         require_once("View/auth/daftar_praktikan.php");
     }
-
-
-    public function prosesAuthAslab($npm,$password){
+    public function prosesAuthAslab($npm,$password)
+    {
         $sql = "select * from aslab where npm='$npm' and password='$password'";
         $query = koneksi()->query($sql);
         return $query->fetch_assoc();
     }
-
-    /** Function authaslab berfungsi untuk authetication aslab */
-    public function authAslab(){
+    /**
+     * function authAslab fungsinya untuk authentication aslab
+     **/
+    public function authAslab()
+    {
         $npm = $_POST['npm'];
         $password = $_POST['password'];
         $data = $this->prosesAuthAslab($npm,$password);
-        if($data){
+
+        if ($data) {
             $_SESSION['role'] = 'aslab';
-            $_SESSION['aslab'] = $data;
+            $_SESSION['aslab'] =$data;
 
             header("location:index.php?page=aslab&aksi=view&pesan=Berhasil Login");
-        }else{
-            header("location:index.php?page=auth&aksi=loginAslab&pesan=Password atau NPM salah");
+        } else {
+            header("location:index.php?page=auth&aksi=loginAslab&pesan=Password atau NPM anda salah!");
         }
     }
-
-
     /**
-     * function untuk cek dari database berdasarkan
+     * function untuk cek data dari database berdasarkan
      * @param String $npm berisi npm
      * @param String $password berisi password
-     */
-    public function prosesAuthPraktikan($npm,$password){
+     **/
+    public function prosesAuthPraktikan($npm,$password)
+    {
         $sql = "select * from praktikan where npm='$npm' and password='$password'";
         $query = koneksi()->query($sql);
         return $query->fetch_assoc();
     }
-
-
-    /** Function authPraktikan berfungsi untuk authetication praktikan */
-    public function authPraktikan(){
+    /**
+     * function authPraktikan untuk authentification praktikan
+     */
+    public function authPraktikan()
+    {
         $npm = $_POST['npm'];
-        $password = $_POST['password'];
-        $data = $this->prosesAuthPraktikan($npm,$password);
-        if($data){
+        $password =$_POST['password'];
+        $data =$this->prosesAuthPraktikan($npm, $password);
+        if ($data) {
             $_SESSION['role'] = 'praktikan';
             $_SESSION['praktikan'] = $data;
 
             header("location:index.php?page=praktikan&aksi=view&pesan=Berhasil Login");
-        }else{
-            header("location:index.php?page=auth&aksi=loginPraktikan&pesan=Password atau NPM salah");
+        } else {
+            header("location:index.php?page=auth&aksi=loginPraktikan&pesan=Password atau NPM salah!");
         }
     }
-    
-    public function logout(){
+    public function logout()
+    {
         session_destroy();
-        header("location:index.php?page=auth&aksi=view&pesan=Berhasil Logout");
+        header("location:index.php?page=auth&aksi=view&pesan=Berhasil Logout!");
+    }
+    /**
+     * function store untuk menambahkan data ke database
+     * @param String $nama berisi data nama
+     * @param String $npm berisi data npm
+     * @param String $no_hp berisi data nomor hp
+     * @param String $password berisi data password
+     */
+    public function prosesStorePraktikan($nama, $npm, $no_hp, $password)
+    {
+        $sql = "INSERT INTO praktikan(nama, npm, nomor_hp, password) VALUE('$nama', '$npm', '$no_hp', '$password')";
+        return koneksi()->query($sql);
+    }
+    /**
+     * function store untuk memproses data untuk ditambahkan
+     * fungsi ini membutuhkan data nama, npm, password, nomor hp dengan metode http request POST
+     */
+    public function storePraktikan()
+    {
+        $nama = $_POST['nama'];
+        $npm = $_POST['npm'];
+        $no_hp = $_POST['no_hp'];
+        $password = $_POST['password'];
+
+        if ($this->prosesStorePraktikan($nama, $npm, $no_hp, $password)){
+            header("location:index.php?page=auth&aksi=view&pesan=Berhasil Daftar");
+        } else {
+            header("location:index.php?page=auth&aksi=daftarPraktikan&pesan=Gagal Daftar");
+        }
     }
 }
 
-//$tes = new AuthModel();
-//var_export($tes->prosesAuthPraktikan('06.2018.1.07000', '123'));
-//die();
